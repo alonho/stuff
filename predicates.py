99ffaa67a21ee69d8bc0ac0991ae8931c6f87204
Alon Horev <alonho@gmail.com>
2011-12-01 16:04:05 +0200

    add doc

--- predicates.py
+++ predicates.py
@@ -1,15 +1,26 @@
 import time
 
 def time_predicate(timeout):
+    """
+    replaces the a common pattern of retrying things within a timeout,
+    what used to go like this:
+
+    finishtime = time.time() + TIMEOUT
+    while time.time() < finishtime:
+        pass
+    
+    replaced with:
+    
+    for _ in time_predicate(TIMEOUT):
+        pass
+    """
     finish_time = time.time() + timeout
     while time.time() < finish_time:
         yield
 
 def test_time_predicate():
+    timeout = 0.3
     stime = time.time()
-    for _ in time_predicate(0.5):
+    for _ in time_predicate(timeout):
         pass
-    assert time.time() - stime >= 0.5 
-
-
-        
+    assert time.time() - stime >= timeout
